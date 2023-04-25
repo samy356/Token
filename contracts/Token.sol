0d7df0212849ddc267f1203530db4fc29a2f9e20
Berwal <300998@QDOM.GLOBAL>
2023-04-25 08:09:54 +0100

    any message

---
+++ contracts/Token.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./ERC20/ERC20.sol";
+
+contract Token is ERC20{
+    constructor() ERC20("Vir", "EKV") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+}
+

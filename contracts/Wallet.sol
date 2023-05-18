1f855110383ad3a94e152a72b6b4e0d53c6c71f7
samy356 <74669954+samy356@users.noreply.github.com>
2023-05-18 22:17:18 +0100

    Add files via upload

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -1,24 +1,31 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
+import "./ERC20/ERC20.sol";
+
 //Single Owner Wallet
 contract EKWallet{
 
-    event deposit(address indexed sender, uint amount);
-    event submitTransaction(address indexed sender, uint txIndex, uint amount, address to);
+    event deposit(address indexed sender, address tokenAddress, uint amount);
+    event balanceof(address indexed tokenaddress, uint amount);
+    event withdraw(address indexed receiver, address tokenAddress, uint amount);
+    event submitTransaction(address indexed sender, address tokenaddress, uint txIndex, uint amount, address to);
     event confirmTransaction(address indexed owner, uint txIndex);
     event executeTransaction(address indexed owner, uint txIndex);
 
-    address public owner;
+    address private owner;
+
+    mapping (address => uint256) private TokenBalance;
 
     struct transaction{
-        address to;
+        address token;
+        address payable to;
         uint value;
         bool txConfirmed;
         bool txExecuted;
     }
 
-    transaction[] public Transactions;
+    transaction[] private Transactions;
     bool private locked;
 
     constructor(){
@@ -52,39 +59,74 @@ contract EKWallet{
         locked = false;
     }
 
+    function OwnerAddress() public view onlyOwner returns(address){
+        return(owner);
+    }
+
     function TransferOwnership(address _to) public onlyOwner{
         owner = _to;
     }
 
-    function Deposit() public payable{
-        (bool success, ) = address(this).call{value: msg.value}(""); 
-        require (success, "Failed to deposit!!!");
-        emit deposit (msg.sender, msg.value);
-    }   // in call funciton, since msg.value is used. Tx is from msg.sender to contract.
+    function ViewTransaction(uint256 _no) public view onlyOwner returns(transaction memory){
+        return(Transactions[_no]);
+    }
+
+    function Deposit(address _tokenAddress, uint _val) public payable{
+        if (_tokenAddress == address(0)){
+            require (msg.value == _val, "Incorrect amount in msg.value");
+            (bool success, ) = address(this).call{value: msg.value}(""); 
+            // in call funciton, since msg.value is used. Tx is from msg.sender to contract.
+            require(success, "Failed to deposit Eth.");
+        }else{
+            ERC20 token = ERC20(_tokenAddress);
+            require (token.allowance(msg.sender, address(this)) >= _val, "Insufficient Allowance!!!");
+            // Allowance is ERC20 function to check if receiver is allowed by sender to use the token.
+            bool success = token.transferFrom(msg.sender, address(this), _val);
+            require (success, "Failed to deposit!!!");
+        }
+        TokenBalance[_tokenAddress] += _val;
+        emit deposit (msg.sender, _tokenAddress, _val);
+    }   
 
     receive() external payable{}
 
-    function Withdraw(uint256 _val) public payable onlyOwner nonRentrant{
-        require (address(this).balance >= _val*(10**18), "Not enough balance");
-        (bool getit, ) = msg.sender.call{value: _val*(10**18)}("");
-        require (getit, "Failed to Withdraw");
-    }   // in call function, since _val (argument) is used. TX is from Contract to msg.sender.
-
-    function Balanceof() public view onlyOwner returns(uint256){
-        return address(this).balance;
+    // if a new ERC20 token have been sent to the wallet contract, then token needs to be mapped to get balance.
+    function MapToken(address _tokenAddress) public onlyOwner{
+        if (_tokenAddress == address(0)){
+            TokenBalance[_tokenAddress] = address(this).balance;
+        }else{
+            ERC20 token = ERC20(_tokenAddress);
+            uint256 bal = token.balanceOf(address(this)); // using ERC20 contract balanceof().
+            TokenBalance[_tokenAddress] = bal;
+        }
     }
 
-    function SubmitTransaction(address _add, uint _value) public
+    function Withdraw(address _tokenAddress, uint256 _val) public payable onlyOwner nonRentrant{
+        require (TokenBalance[_tokenAddress] >= _val, "Not enough balance");
+        ERC20 token = ERC20(_tokenAddress);
+        (bool getit ) = token.transfer(msg.sender, _val);
+        require (getit, "Failed to Withdraw");
+        TokenBalance[_tokenAddress]-=_val;
+        emit withdraw(msg.sender, _tokenAddress, _val);
+    }   // in call function, since _val (argument) is used. TX is from Contract to msg.sender.
+
+    function Balanceof(address _tokenAddress) public onlyOwner returns(uint256){
+        emit balanceof(_tokenAddress, TokenBalance[_tokenAddress]);
+        return TokenBalance[_tokenAddress];
+    }
+
+    function SubmitTransaction(address _tokenaddress, address payable _add, uint _value) external
     onlyOwner{
         uint txIndex = Transactions.length;
         Transactions.push (
             transaction(
+                _tokenaddress,
                 _add,
                 _value*(10**18),
                 false,
                 false
         ));
-        emit submitTransaction(msg.sender, txIndex, _value*(10**18), _add);
+        emit submitTransaction(msg.sender, _tokenaddress, txIndex, _value*(10**18), _add);
     }
 
     function ConfirmTransaction(uint _txIndex) public
@@ -101,15 +143,19 @@ contract EKWallet{
     txConfirmed (_txIndex)
     txNotExecuted (_txIndex)
     nonRentrant {
-        require (Transactions[_txIndex].value <= address(this).balance, "Not enough balance!!");
+        ERC20 token = ERC20(Transactions[_txIndex].token);
+        uint _txVal = Transactions[_txIndex].value;
+        require (_txVal <= TokenBalance[Transactions[_txIndex].token], "Not enough balance!!");
+        bool success = token.transferFrom(address(this), Transactions[_txIndex].to, _txVal);
+        require (success, "TX failed to Execute");
 
         // If you want to use call. No gas limit in call
-        (bool exec, ) = Transactions[_txIndex].to.call{value: Transactions[_txIndex].value}("");
-        require (exec, "Transaction didn't got executed!!");    
-
+        // (bool exec, ) = Transactions[_txIndex].to.call{value: Transactions[_txIndex].value}("");
+        // require (exec, "Transaction didn't got executed!!");    
         //If you want to use transfer. There is gas limit in transfer and it throw an exception when fail to execute
         //Transactions[_txIndex].to.transfer(Transactions[_txIndex].value);
 
+        TokenBalance[Transactions[_txIndex].token] -= _txVal;
         Transactions[_txIndex].txExecuted = true;
         emit executeTransaction (msg.sender, _txIndex);
     }
